8efc20b1ae1fcb43c139216e75bd8e94a91163af
andresct2010 <61914964+andresct2010@users.noreply.github.com>
2022-08-13 09:27:46 -0500

    Update app.py
    MSFT

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello_world():
 
 @app.route("/get-price/<ticker>")
 def get_price(ticker):
-    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/MSFT?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
     response = requests.get(url)
     result =response.json()
     print(result)
